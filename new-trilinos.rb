1ad1afc008d9903ae2e82f545a4ebaa78e395c93
Dominique Orban <dominique.orban@gmail.com>
2015-01-15 23:53:18 -0500

    Make deps recommended.

--- new-trilinos.rb
+++ new-trilinos.rb
@@ -44,11 +44,11 @@ class NewTrilinos < Formula
 
   # Experimental TPLs (all but tbb is turned off by default):
   #-depends_on "eigen"        => :optional # Intrepid_test_Discretization_Basis_HGRAD_TET_Cn_FEM_ORTH_Test_02 fails to build
-  depends_on "hypre"        => [:optional] + ((build.with? :mpi) ? ["with-mpi"] : []) # EpetraExt tests fail to compile
-  depends_on "glpk"         => :optional
-  depends_on "hdf5"         => [:optional] + ((build.with? :mpi) ? ["with-mpi"] : [])
+  depends_on "hypre"        => [:recommended] + ((build.with? :mpi) ? ["with-mpi"] : []) # EpetraExt tests fail to compile
+  depends_on "glpk"         => :recommended
+  depends_on "hdf5"         => [:recommended] + ((build.with? :mpi) ? ["with-mpi"] : [])
   depends_on "tbb"          => :recommended
-  depends_on "glm"          => :optional
+  depends_on "glm"          => :recommended
 
   #-depends_on "lemon"        => :optional # lemon is currently built as executable only, no libraries
   #-depends_on "cask"         => :optional # cask  is currently built as executable only, no libraries
